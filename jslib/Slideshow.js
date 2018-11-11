fc06ab5cb26c8604165170a5bbc889462910f1ce
David Evenson <evenson6@msu.edu>
2018-11-10 21:41:21 -0600

    Set up JavaScript. Created slideshow for project section.

---
+++ jslib/Slideshow.js
@@ -0,0 +1,28 @@
+var currentSlide = 0;
+
+function nextSlide(n) {
+    showSlides(currentSlide += n);
+    console.log(currentSlide);
+}
+
+function getSlide(n) {
+    showSlides(currentSlide = n);
+    console.log(currentSlide);
+}
+
+function showSlides(n) {
+    console.log("start");
+    var slides = document.getElementsByClassName("ProjectSlide");
+    if (n > slides.length - 1) {
+        currentSlide = 0;
+    }
+    if (n < 0) {
+        currentSlide = slides.length - 1;
+    }
+    var i;
+    for(i=0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    console.log(i);
+    slides[currentSlide].style.display = "block";
+}
